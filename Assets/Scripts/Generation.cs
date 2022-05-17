a96c649d35207b8e4c3176846002c7f4a22e91ad
Samuel Adlard <72796569+SamuelAdlard@users.noreply.github.com>
2022-05-17 10:39:47 +1000

    Initial commit

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -5,77 +5,64 @@ using UnityEngine;
 public class Generation : MonoBehaviour
 {
     public GameObject Tile;
-    public Tile[,] Tiles = new Tile[10,10];
+    public Material[] materials;
+    public int seed;
+    public Tile[,] Tiles = new Tile[100,100];
+    public enum types
+    {
+        sea,
+        desert,
+        grass,
+        forest,
+        freshwater,
+        mountain
+    }
+
     void Start()
     {
-        for (int x = 0; x < Tiles.GetLength(0); x++)
+        for (int x = 0; x < 100; x++)
         {
-            for (int y = 0; y < Tiles.GetLength(1); y++)
+            for (int y = 0; y < 100; y++)
             {
+
+                Tiles[x, y] = new Tile();
                 Tiles[x, y].tile = Instantiate(Tile, new Vector3(x, 0, y), Quaternion.identity);
-                Tiles[x, y].tile.name = x.ToString() + y.ToString();
+                Tiles[x, y].tile.name = x.ToString() + y.ToString() + " " + Mathf.PerlinNoise(x * 0.025f, y * 0.025f);
+                
+                if (Mathf.PerlinNoise((x + seed) * 0.05f, (y + seed) * 0.05f) <= 0.4f)
+                {
+                    Tiles[x, y].type = types.sea;
+                    
+                }
+                else if (Mathf.PerlinNoise((x + seed) * 0.05f, (y + seed) * 0.05f) > 0.4f && Mathf.PerlinNoise((x + seed) * 0.05f, (y + seed) * 0.05f) < 0.6f)
+                {
+                    Tiles[x, y].type = types.desert;
+                    
+                }
+                else
+                {
+                    Tiles[x, y].tile.transform.localScale = new Vector3(1, 2, 1);
+                    Tiles[x, y].type = types.mountain;
+                }
+                Tiles[x, y].tile.GetComponent<MeshRenderer>().material = materials[(int)Tiles[x, y].type];
             }
         }
     }
 
-    int RandomType(int x, int y)
-    {
-        int RandomNumber = 0;
-        if (x == 0 && y == 0)
-        {
-            RandomNumber = Random.Range(0, 2);
-            return RandomNumber;
-        }
-        else if(x == 0)
-        {
-            if (Tiles[x, y - 1].type == 0)
-            {
-                RandomNumber = Random.Range(0, 5);
-                if (RandomNumber >= 3)
-                { 
-                    return 0;
-                }
-                else
-                {
-                    return 1;
-                }
-            }
-            else if(Tiles[x, y - 1].type == 5)
-            {
-                RandomNumber = Random.Range(0, 2);
-                if (RandomNumber == 0)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 5;
-                }
-            }
-            else
-            {
-                RandomNumber = Random.Range(0, 5);
-                if ()
-                {
+    
 
-                }
-            }
-        }
-
-    }    
+    
    
 }
 
 public class Tile
 {
     public GameObject tile;
-    public int type = 0;
-    //0 = sea
-    //1 = desert
-    //2 = grass
-    //3 = forest
-    //4 = fresh water
-    //5 = mountain
+    public Generation.types type = 0;
+
+
+
+    public float waterreductionRate = 1;
     public float water = 0f;
-    
+    public float temperature = 20f;
 }
