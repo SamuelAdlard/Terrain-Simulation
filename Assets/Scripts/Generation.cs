d8c242accfd527937ac2f13824ddf210e91d0c77
Samuel Adlard <72796569+SamuelAdlard@users.noreply.github.com>
2022-05-19 09:50:55 +1000

    cloud

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -11,6 +11,8 @@ public class Generation : MonoBehaviour
     public int size = 50;
     public Tile[,] Tiles = new Tile[50,50];
     public Cloud[,] Clouds = new Cloud[50, 50];
+    float nextTick = 0;
+    public float delay = 2;
     public enum types
     {
         sea,
@@ -32,7 +34,11 @@ public class Generation : MonoBehaviour
                 Clouds[x, y].cloud.SetActive(false);
                 Tiles[x, y] = new Tile();
                 Tiles[x, y].tile = Instantiate(Tile, new Vector3(x, 0, y), Quaternion.identity);
-                Tiles[x, y].temperature = y * 0.4f + 10 + Random.Range(-10, 11);
+                Tiles[x, y].temperature = y * 0.05f + 10 + Random.Range(-5, 6);
+                if (Tiles[x, y].type == types.sea)
+                {
+                    Tiles[x, y].temperature += 5;
+                }
                 Tiles[x, y].tile.name = x.ToString() + y.ToString() + " " + Mathf.PerlinNoise(x * 0.025f, y * 0.025f) + " " + Tiles[x, y].temperature;
                
                 if (Mathf.PerlinNoise((x + seed) * 0.05f, (y + seed) * 0.05f) <= 0.4f)
@@ -58,36 +64,54 @@ public class Generation : MonoBehaviour
     private void Update()
     {
 
-        if (Input.GetKeyDown("c"))
+        if (nextTick < Time.time)
         {
-            SimulateClouds();
+            print("Hi");
+            nextTick = Time.time + delay;
+            Simulate();
         }
        
         
     }
 
-    void SimulateClouds()
+    void Simulate()
     {
         for (int x = 0; x < size; x++)
         {
             for (int y = 0; y < size; y++)
             {
-                print(Clouds[x, y].isCloud + " " + x + " " + y);
-                if (Clouds[x, y].isCloud == true)
+                
+                if (Clouds[x, y].cloud.activeSelf == true)
                 {
+                    if (Tiles[x, y].type != types.sea)
+                    {
+                        if(Clouds[x, y].water > 2)
+                        {
+                            Tiles[x, y].water += 2;
+                            Clouds[x, y].water -= 2;
+                        }
+                        else
+                        {
+                            Tiles[x, y].water += Clouds[x, y].water;
+                            Clouds[x, y].water = 0;
+                        }
+                    }
+                    
                     Vector2[] neighbours = GetNeighbours(x, y);
                     Vector2 bestPos = new Vector2(x, y);
                     float lowestTemp = 100;
                     foreach (Vector2 neighbour in neighbours)
                     {
-                        if (neighbour.x < size + 1 && Tiles[Mathf.RoundToInt(neighbour.x), Mathf.RoundToInt(neighbour.y)].temperature >= lowestTemp)
+                        
+                        if (neighbour.x < size + 1 && Tiles[Mathf.RoundToInt(neighbour.x), Mathf.RoundToInt(neighbour.y)].temperature <= lowestTemp && Tiles[Mathf.RoundToInt(neighbour.x), Mathf.RoundToInt(neighbour.y)].type != types.mountain)
                         {
                             
+
                             bestPos = neighbour;
                             lowestTemp = Tiles[Mathf.RoundToInt(neighbour.x), Mathf.RoundToInt(neighbour.y)].temperature;
                         }
                     }
-                    print(bestPos + "(" + y + ", " + x + ")");
+                    
                     Clouds[x, y].cloud.SetActive(false);
                     Clouds[Mathf.RoundToInt(bestPos.x), Mathf.RoundToInt(bestPos.y)].water += Clouds[x, y].water;
                     Clouds[Mathf.RoundToInt(bestPos.x), Mathf.RoundToInt(bestPos.y)].isCloud = true;
@@ -97,19 +121,33 @@ public class Generation : MonoBehaviour
 
                 if (Tiles[x, y].type == types.sea)
                 {
-                    Clouds[x, y].water += Tiles[x, y].temperature * 0.05f;
-                    if (Clouds[x, y].water > 2.5f)
-                    {
-                        Clouds[x, y].isCloud = true;
-                        Clouds[x, y].cloud.SetActive(true);
-                        
-                    }
-                    else
-                    {
-                        Clouds[x, y].isCloud = false;
-                        Clouds[x, y].cloud.SetActive(false);
-                    }
+                    Clouds[x, y].water += Tiles[x, y].temperature * 0.05f + Random.Range(-15,6);
+                    
                 }
+
+                if (Clouds[x, y].water > 2.5f)
+                {
+                    Clouds[x, y].isCloud = true;
+                    Clouds[x, y].cloud.SetActive(true);
+
+                }
+                else
+                {
+                    Clouds[x, y].isCloud = false;
+                    Clouds[x, y].cloud.SetActive(false);
+                }
+
+                if (Tiles[x, y].water >= 3)
+                {
+                    Tiles[x, y].type = types.grass;
+                    
+                }
+                else if(Tiles[x, y].water >= 5)
+                {
+                    Tiles[x, y].type = types.forest;
+                }
+                Tiles[x, y].temperature = y * 0.05f + 10 + Random.Range(-5, x);
+                Tiles[x, y].tile.GetComponent<MeshRenderer>().material = materials[(int)Tiles[x, y].type];
             }
         }
     }
